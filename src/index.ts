24b5d5358bec230a654ab7723de514c9a9668194
Gino <2dvrstudios@gmail.com>
2019-11-28 09:36:11 -0800

    clean up

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./api/Node";
-export * from "./api/Player";
-export * from "./api/RESTManager";
-
-export * from "./store/Player";
-export * from "./store/Node";
-
-export * from "./Manager";
-export * from "./interfaces/Entities";
